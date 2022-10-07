94ee9883f8e66c61497e8d38f3cbc5984b426c2c
fernando <fer20comp@gmail.com>
2022-10-07 09:36:40 -0300

    project creation in Xcode

---
+++ WhyNot/WhyNot/WhyNotApp.swift
@@ -0,0 +1,17 @@
+//
+//  WhyNotApp.swift
+//  WhyNot
+//
+//  Created by Fernando Santos on 07/10/22.
+//
+
+import SwiftUI
+
+@main
+struct WhyNotApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
