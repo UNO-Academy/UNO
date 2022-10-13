a1e9e7be91b274a7f3d516f5c00612511b6b3600
LarissaEscaliante <larissastefano12@gmail.com>
2022-10-13 10:20:31 -0300

    Created structure to project

---
+++ WhyNot/WhyNot/data/Repository.swift
@@ -0,0 +1,8 @@
+//
+//  Repository.swift
+//  WhyNot
+//
+//  Created by Larissa Gomes de Stefano Escaliante on 13/10/22.
+//
+
+import Foundation
---
+++ WhyNot/WhyNot/data/remoteDatabase/API/ExperienceAPI.swift
@@ -0,0 +1,8 @@
+//
+//  ExperienceAPI.swift
+//  WhyNot
+//
+//  Created by Larissa Gomes de Stefano Escaliante on 13/10/22.
+//
+
+import Foundation
