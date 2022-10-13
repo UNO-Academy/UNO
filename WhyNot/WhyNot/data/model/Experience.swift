2b7fe9755b5026983dc490b8de9ff159a0f5cd62
LarissaEscaliante <larissastefano12@gmail.com>
2022-10-13 11:12:51 -0300

    Implemented Experience and ExperienceAPI

--- WhyNot/WhyNot/data/model/Experience.swift
+++ WhyNot/WhyNot/data/model/Experience.swift
@@ -6,3 +6,13 @@
 //
 
 import Foundation
+
+class Experience {
+    let id: String
+    let name: String
+    let description: String
+    let effor: Int
+    let duration: Int
+    let category: String
+    let isActive: Bool
+}
--- WhyNot/WhyNot/data/remoteDatabase/API/ExperienceAPI.swift
+++ WhyNot/WhyNot/data/remoteDatabase/API/ExperienceAPI.swift
@@ -6,3 +6,26 @@
 //
 
 import Foundation
+
+class ExperienceAPI {
+
+    private let collectionName: String = "Experience"
+    private let crudService: AnyClass?
+
+    func getActiveExperiences() async throws -> [Experience] {
+
+        return crudService.getFilteredItems(
+            collectionName: collectionName,
+            field: "isActive",
+            value: true
+        )
+    }
+
+    func getExperiencesByIdList() async throws -> [Experience] {
+
+        return crudService.getItemsByIdList(
+            collectionName: collectionName,
+            idList: [String]
+        )
+    }
+}
