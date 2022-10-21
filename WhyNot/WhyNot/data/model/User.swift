ac5ec530bd765490d323725842cd52efef343a6f
Luiz Gustavo Silva Aguiar <l.gustavo092000@gmail.com>
2022-10-21 09:52:51 -0300

    added user model

---
+++ WhyNot/WhyNot/data/model/User.swift
@@ -0,0 +1,19 @@
+//
+//  User.swift
+//  WhyNot
+//
+//  Created by Luiz Gustavo Silva Aguiar on 21/10/22.
+//
+
+import FirebaseFirestoreSwift
+import Foundation
+
+struct User: Decodable, UpdatableIdentifiable {
+    @DocumentID var id: String?
+    let name: String
+    let profilePicture: String
+    let lastPictureUpdate: Date
+    let friendID: [String]
+    let mementosID: [String]
+    let interestExperiencesID: [String]
+}
