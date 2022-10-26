324b1a175174141b25b3cea1d8f05848ed2ec937
fernando <fer20comp@gmail.com>
2022-10-26 09:44:07 -0300

    fix coments

--- WhyNot/WhyNot/WhyNotApp.swift
+++ WhyNot/WhyNot/WhyNotApp.swift
@@ -12,6 +12,15 @@ import Firebase
 struct WhyNotApp: App {
     var body: some Scene {
         WindowGroup {
+            ContentView().onAppear(perform: {
+                FirebaseApp.configure()
+            })
         }
+    }
+}
+
+struct ContentView: View {
+    var body: some View {
+        Text("Essa contentView só extiste pra larissa não surtar")
     }
 }
--- WhyNot/WhyNot/data/remoteDatabase/StorageServices.swift
+++ WhyNot/WhyNot/data/remoteDatabase/StorageServices.swift
@@ -10,22 +10,17 @@ import FirebaseStorage
 import Foundation
 
 class StorageService {
-    
-    static let shared = StorageService()
+
     private var group: DispatchGroup?
-    
-    private init() {
-        FirebaseApp.configure()
-    }
-    
+
     func uploadImage(storageRef: Storage, image: Data) {
         let riversRef = storageRef.reference().child("images/\(UUID().uuidString).jpg")
         riversRef.putData(image, metadata: nil)
     }
-    
+
     func downloadImage(storageRef: Storage, path: String, completion: @escaping (Result<Data, Error>) -> Void) {
         let imageRef = storageRef.reference(withPath: path)
-        
+
         imageRef.getData(maxSize: Sizes.maxSize.rawValue * Sizes.maxSize.rawValue) { (data, error) in
             if let error = error {
                 completion(.failure(error))
@@ -34,16 +29,16 @@ class StorageService {
             }
         }
     }
-    
+
     func downloadImages(storageRef: Storage, paths: [String], completion: @escaping ([Data]) -> Void) {
         group = DispatchGroup()
         for _ in paths {
             group?.enter()
         }
-        
+
         var imagesData: [Data] = []
         let semaphore = DispatchSemaphore(value: 1)
-        
+
         for path in paths {
             downloadImage(storageRef: storageRef, path: path) { [weak self] result in
                 switch result {
@@ -57,9 +52,8 @@ class StorageService {
                 self?.group?.leave()
             }
         }
-        
+
         group?.notify(queue: DispatchQueue.global()) {
-            print("Terminei de baixar os dados das imagens")
             completion(imagesData)
         }
     }
@@ -67,19 +61,4 @@ class StorageService {
 
 enum Sizes: Int64 {
     case maxSize = 1024
-}
-
-extension StorageReference {
-    
-    func getDataAsync(maxSize: Int64) async throws -> Data {
-        return try await withCheckedThrowingContinuation({ continuation in
-            getData(maxSize: maxSize) { data, error in
-                if error == nil && data != nil {
-                    continuation.resume(returning: data!)
-                } else {
-                    continuation.resume(throwing: error!)
-                }
-            }
-        })
-    }
 }
