ed9ab93f9ba42213b9305a626fc2146d000add4c
fernando <fer20comp@gmail.com>
2022-10-31 10:14:36 -0300

    spaces added

--- WhyNot/WhyNot/ContentView.swift
+++ WhyNot/WhyNot/ContentView.swift
@@ -11,6 +11,8 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         Text("Essa contentView só extiste pra larissa não surtar")
-            .font(Font.custom(CustomFonts.LYLSS, size: FontSize.largeTitle)).foregroundColor(Color.CustomColors.CardTitle)
+            .font(Font.custom(CustomFonts.LYLSS, size: FontSize.largeTitle))
+            .foregroundColor(Color.CustomColors.CardTitle)
+            .padding(Space.halfSpace)
     }
 }
