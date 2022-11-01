0ae528c8f03db57493df8cf4426055a4a76eacd3
Luiz Gustavo Silva Aguiar <l.gustavo092000@gmail.com>
2022-11-01 13:44:12 -0300

    added signIn and signOut functions

--- WhyNot/WhyNot/data/remoteDatabase/AuthenticationManager.swift
+++ WhyNot/WhyNot/data/remoteDatabase/AuthenticationManager.swift
@@ -9,10 +9,28 @@ import FirebaseAuth
 
 class AuthenticationManager {
     let auth = Auth.auth()
-    
+
     func createAccount(_ email: String, _ password: String) async throws -> AuthDataResult {
         do {
             return try await auth.createUser(withEmail: email, password: password)
+        } catch {
+            throw error
+        }
+    }
+
+    func signIn(_ email: String, _ password: String, completion: @escaping (Result<String, Error>) -> Void ) {
+        auth.signIn(withEmail: email, password: password) { authResult, error in
+            if let error = error {
+                completion(.failure(error))
+            }
+            let userID = authResult?.user.uid ?? "Unknown"
+            completion(.success(userID))
+        }
+    }
+    
+    func signOut() throws {
+        do {
+            try auth.signOut()
         } catch {
             throw error
         }
