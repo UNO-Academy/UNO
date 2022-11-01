120df690e50a7217f5a623016b6c390b3ac10d2e
Luiz Gustavo Silva Aguiar <l.gustavo092000@gmail.com>
2022-11-01 11:37:15 -0300

    implemented create

---
+++ WhyNot/WhyNot/data/remoteDatabase/AuthenticationManager.swift
@@ -0,0 +1,20 @@
+//
+//  AuthenticationManager.swift
+//  WhyNot
+//
+//  Created by Luiz Gustavo Silva Aguiar on 01/11/22.
+//
+
+import FirebaseAuth
+
+class AuthenticationManager {
+    let auth = Auth.auth()
+    
+    func createAccount(_ email: String, _ password: String) async throws -> AuthDataResult {
+        do {
+            return try await auth.createUser(withEmail: email, password: password)
+        } catch {
+            throw error
+        }
+    }
+}
