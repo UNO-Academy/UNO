ccc6612358ac7bcd3edf13a2f6072cb6854e2e08
LarissaEscaliante <larissastefano12@gmail.com>
2022-11-03 10:32:36 -0300

    merge

--- WhyNot/WhyNot/DesignSystem/Colors.swift
+++ WhyNot/WhyNot/DesignSystem/Colors.swift
@@ -18,4 +18,18 @@ extension Color {
         static var TitleColor: Color { return Color("TitleColor") }
         static var TabBarSymbol: Color { return Color("TabBarSymbol") }
     }
+
+    enum PrimaryPallet {
+        static var red: Color { return Color("primaryRed") }
+        static var yellow: Color { return Color("primaryYellow") }
+        static var green: Color { return Color("primaryGreen") }
+        static var disable: Color { return Color("primaryDisable") }
+    }
+
+    enum SecondaryPallet {
+        static var red: Color { return Color("secondaryRed") }
+        static var yellow: Color { return Color("secondaryYellow") }
+        static var green: Color { return Color("secondaryGreen") }
+        static var disable: Color { return Color("secondaryDisable") }
+    }
 }
