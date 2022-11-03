9aca08d2cef4644f2fcb7a2001b98410c6194304
LarissaEscaliante <larissastefano12@gmail.com>
2022-11-03 10:30:30 -0300

    merge develop

---
+++ WhyNot/WhyNot/Components/CategoryIcon.swift
@@ -0,0 +1,40 @@
+//
+//  CategoryIcon.swift
+//  WhyNot
+//
+//  Created by Luís Filipe Nascimento on 01/11/22.
+//
+
+import SwiftUI
+
+struct CategoryIcon: View {
+
+    let type: ExperienceType
+    @Binding var isEnable: Bool
+
+    var body: some View {
+        Image(type.rawValue)
+            .resizable()
+            .aspectRatio(1, contentMode: .fit)
+            .background(
+                RoundedRectangle(cornerRadius: Radius.defaultRadius)
+                    .fill(isEnable ? type.secondaryColor : Color.SecondaryPallet.disable)
+            )
+    }
+}
+
+private struct CategoryIconPreviewHelper: View {
+
+    @State var isEnable: Bool
+
+    var body: some View {
+        CategoryIcon(type: .adventure, isEnable: $isEnable)
+    }
+
+}
+
+struct CategoryIcon_Previews: PreviewProvider {
+    static var previews: some View {
+        CategoryIconPreviewHelper(isEnable: false)
+    }
+}
