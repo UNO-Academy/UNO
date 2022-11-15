a978cf807d95d84531eb52cddb68cc45c70c16e3
Luiz Gustavo Silva Aguiar <l.gustavo092000@gmail.com>
2022-11-15 18:15:22 -0300

    fix decode

--- WhyNot/WhyNot/Extension/DocumentSnapshot+Extension.swift
+++ WhyNot/WhyNot/Extension/DocumentSnapshot+Extension.swift
@@ -6,16 +6,14 @@
 //
 
 import FirebaseFirestore
+import FirebaseFirestoreSwift
 
 extension DocumentSnapshot {
     func toObject<T: Decodable>() throws -> T  where T: UpdatableIdentifiable, T.ID == String? {
         do {
-            print(self.data())
-            var object = try Firestore.Decoder().decode(T.self, from: self.data())
-            object.id = self.documentID
+            var object = try self.data(as: T.self)
             return object
         } catch {
-            print("Não foi possível decodificar o documento")
             throw error
         }
     }
