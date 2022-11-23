d61b1f9cf715a7631a748bbb50debe7337c3e48a
LarissaEscaliante <larissastefano12@gmail.com>
2022-11-23 09:12:01 -0300

    pare de me atormentar por favor

--- WhyNot/WhyNot/Data/Repository/ExperienceRepository.swift
+++ WhyNot/WhyNot/Data/Repository/ExperienceRepository.swift
@@ -1,12 +1,6 @@
-//
-//  ExperienceRepository.swift
-//  WhyNot
-//
-//  Created by Larissa Gomes de Stefano Escaliante on 17/11/22.
-//
-
-import Foundation
+import FirebaseFirestore
 
 protocol ExperienceRepository {
+
     func fetchExperiences() async throws -> [Experience]
 }
